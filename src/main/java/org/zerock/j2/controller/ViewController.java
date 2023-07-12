0c5cad0f5637083d55add4f01dc74ac683eb6601
sj <sujung033131@gmail.com>
2023-07-12 17:52:07 +0900

    nginx없이 이미지 뜨게하는 코드

--- src/main/java/org/zerock/j2/controller/ViewController.java
+++ src/main/java/org/zerock/j2/controller/ViewController.java
@@ -1,2 +1,37 @@
-package org.zerock.j2.controller;public class ViewController {
+package org.zerock.j2.controller;
+
+import java.io.File;
+import java.nio.file.Files;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+@Controller
+public class ViewController {
+
+    @Value("${org.zerock.upload.path}")// import 시에 springframework으로 시작하는 Value
+    private String uploadPath;
+
+        @GetMapping("/view/{fileName}")
+    public ResponseEntity<Resource> viewFileGET(@PathVariable String fileName){
+
+        Resource resource = new FileSystemResource(uploadPath+File.separator + fileName);
+        String resourceName = resource.getFilename();
+        HttpHeaders headers = new HttpHeaders();
+
+        try{
+            headers.add("Content-Type", Files.probeContentType( resource.getFile().toPath() ));
+        } catch(Exception e){
+            return ResponseEntity.internalServerError().build();
+        }
+        return ResponseEntity.ok().headers(headers).body(resource);
+    }
 }
+
